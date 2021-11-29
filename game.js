66d7b93652e20076737ab10bddec8411f95a51f8
arkch99 <arko1999@gmail.com>
2021-11-29 12:20:26 +0530

    Add boundary and overlap checks

--- game.js
+++ game.js
@@ -13,13 +13,16 @@ const Ship = function(length) {
 
 const Gameboard = function() {
 	return {
-		mat: Array(10).fill(Array(10).fill(null)),
+		mat: [...Array(10)].map(row => Array(10).fill(null)),
 		placeShip: function(coords, length, dir) {
 			// dir = 0 => horz; dir = 1 => vert
 			let x = coords[0];
 			let y = coords[1];
-			// let x_incr = 1 - dir;
-			// let y_incr = dir;
+			
+			if(this.mat[x][y]) // must be null - else another ship starts here.
+			{
+				return false;
+			}
 			if(dir == 1) // vert
 			{
 				if(x + length > 10) // within bounds
@@ -33,22 +36,36 @@ const Gameboard = function() {
 				{
 					return false;
 				}
-			}
-			this.mat[x][y] = Ship(length);
+			}						
 			if(dir == 1) // vert
 			{
+				for(let i = x + 1; i < x + length; i++)
+				{
+					if(this.mat[i][y]) // occupied
+					{
+						return false;
+					}
+				}
 				for(let i = x + 1; i < x + length; i++)
 				{
 					this.mat[i][y] = [x, y]; // coords of parent ship
 				}
 			}
-			else //horz
+			else // horz
 			{
+				for(let i = y + 1; i < y + length; i++)
+				{
+					if(this.mat[x][i]) // occupied
+					{
+						return false;
+					}
+				}		
 				for(let i = y + 1; i < y + length; i++)
 				{
 					this.mat[x][i] = [x, y]; // coords of parent ship
 				}
 			}
+			this.mat[x][y] = Ship(length);
 			return true;
 		}
 	};
--- game.test.js
+++ game.test.js
@@ -26,37 +26,66 @@ describe('Ship', () => {
 	});
 });
 
-describe('Gameboard', () => {
-	beforeAll(() => {
-		board = game.Gameboard();
+describe('Gameboard', () => {	
+	describe('basic', () => {
+		beforeAll(() => {
+			board = game.Gameboard();
+		});
+		test('is created', () => {
+			expect(board).not.toBeUndefined();
+		});
+		test('has proper dimensions', () => {
+			expect(board.mat.length).toBe(10) && expect(board.mat[0].length).toBe(10);
+		});
+		test('can place ships horizontally within bounds', () => {
+			let placed = board.placeShip([4, 5], 3, 0); // ship of length 3, horz, at (4,5)
+			let correctlyAssigned = true;
+			expect(placed).toBe(true);
+	
+			for(let i = 6; i < 8; i++)
+			{
+				correctlyAssigned = correctlyAssigned && (board.mat[4][i][0] == 4 && board.mat[4][i][1] == 5)
+			}
+			
+			expect(correctlyAssigned).toBe(true);
+		});
+		test('can place ships vertically within bounds', () => {
+			let placed = board.placeShip([7, 7], 3, 1);		
+			expect(placed).toBe(true);
+	
+			let correctlyAssigned = true;
+			for(let i = 8; i < 10; i++)
+			{
+				correctlyAssigned = correctlyAssigned && (board.mat[i][7][0] == 7 && board.mat[i][7][1] == 7);
+			}
+			expect(correctlyAssigned).toBe(true);
+		});
 	});
-	test('is created', () => {
-		expect(board).not.toBeUndefined();
+	describe('boundary and overlap check', () => {
+		beforeAll(() => {
+			board = game.Gameboard();
+			board.placeShip([1, 2], 5, 0);
+			board.placeShip([4, 5], 3, 0);
+			board.placeShip([5, 2], 2, 1);
+			board.placeShip([7, 7], 3, 1);
+		});
+		test('does not allow placing ships horizontally out of bounds', () => {
+			expect(board.placeShip([3, 8], 3, 0)).toBe(false);
+		});
+		test('does not allow placing ships vertically out of bounds', () => {
+			expect(board.placeShip([6, 3], 5, 1)).toBe(false);
+		});
+		test('does not allow orthogonal overlap - source vertical', () => {
+			expect(board.placeShip([0, 3], 3, 1)).toBe(false);
+		});
+		test('does not allow orthogonal overlap - source horizontal', () => {
+			expect(board.placeShip([6, 1], 5, 0)).toBe(false);
+		});
+		test('does not allow linear overlap - horizontal', () => {
+			expect(board.placeShip([4, 4], 3, 0)).toBe(false);
+		});
+		test('does not allow linear overlap - vertical', () => {
+			expect(board.placeShip([7, 7], 2, 1)).toBe(false);
+		});
 	});
-	test('has proper dimensions', () => {
-		expect(board.mat.length).toBe(10) && expect(board.mat[0].length).toBe(10);
-	});
-	test('can place ships horizontally within bounds', () => {
-		let placed = board.placeShip([4, 5], 3, 0); // ship of length 3, horz, at (4,5)
-		let correctlyAssigned = true;
-		expect(placed).toBe(true);
-
-		for(let i = 6; i < 8; i++)
-		{
-			correctlyAssigned = correctlyAssigned && (board.mat[4][i][0] == 4 && board.mat[4][i][1] == 5)
-		}
-		
-		expect(correctlyAssigned).toBe(true);
-	});
-	test('can place ships vertically within bounds', () => {
-		let placed = board.placeShip([7, 7], 3, 1);		
-		expect(placed).toBe(true);
-
-		let correctlyAssigned = true;
-		for(let i = 8; i < 10; i++)
-		{
-			correctlyAssigned = correctlyAssigned && (board.mat[i][7][0] == 7 && board.mat[i][7][1] == 7);
-		}
-		expect(correctlyAssigned).toBe(true);
-	})
 })
