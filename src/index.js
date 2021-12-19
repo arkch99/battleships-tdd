b08b46aa8a5d4fe5f04226663d7ed9778c0d3c9e
arkch99 <arko1999@gmail.com>
2021-12-19 15:43:53 +0530

    Render board without ships; center align with flex

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ function drawBoards(player1, player2)
 	const player1BoardContainer = document.getElementById('own-board');
 	const player2BoardContainer = document.getElementById('opp-board');
 
+	document.getElementById('player-ships').textContent = player1.board.nShips;
+	document.getElementById('opp-ships').textContent = player2.board.nShips;
 	player1BoardContainer.style.gridTemplateRows = 'repeat(10, auto)';
 	player1BoardContainer.style.gridTemplateColumns = 'repeat(10, auto)';
 
@@ -39,7 +41,12 @@ function init(name)
 
 	document.getElementById('info-get-screen').style.display = 'none';
 	document.getElementById('game-screen').style.display = 'block';
-
+	document.querySelectorAll('.player-name').forEach(ele => {
+		ele.textContent = name;
+	});
+	document.querySelectorAll('.opp-name').forEach(ele => {
+		ele.textContent = 'AI';
+	});
 	playerHuman.board.setupBoard();
 	playerAI.board.setupBoard();
 
@@ -48,7 +55,7 @@ function init(name)
 
 function getName()
 {
-	const name = document.getElementById('player-name').textContent;	
+	const name = document.getElementById('player-name').value;	
 	init(name);
 }
 
